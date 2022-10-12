bddf0b26912d1bb738793a0cc839a0f95dc0eb48
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2022-10-11 18:48:00 -0700

    Q1B

---
+++ q1b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int N=5, M=5;
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M - (2 * i); j++) {
+            cout << " (" << i << "," << j << ") ";
+        }
+        cout << endl;
+    }
+}
